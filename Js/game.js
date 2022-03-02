d57e3ac4961aa02fdeb3483f75f9ac2ce4d5cce0
Harmeto <bad.thg@gmail.com>
2022-03-02 16:14:12 -0300

    First Upload
    Game with JS - Html - Css.
    
    Work in progress!

---
+++ Js/game.js
@@ -0,0 +1,198 @@
+//evento load permite cargar assets correctamente antes de funcionar el animated
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+    let enemies = [];
+    //class para ingresar botones
+    class InputHandler {
+        constructor() {
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                if ((e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowRight' ||
+                        e.key === 'ArrowLeft') &&
+                    this.keys.indexOf(e.key) === -1) {
+                    this.keys.push(e.key);
+
+                }
+            });
+            window.addEventListener('keyup', e => {
+                if (e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowRight' ||
+                    e.key === 'ArrowLeft') {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+            });
+        }
+    }
+    //class para jugador
+    class Player {
+        //constructor con parametros para player
+        constructor(gameWidth, gameHeight) {
+                this.gameWidth = gameWidth;
+                this.gameHeight = gameHeight;
+                this.width = 200;
+                this.height = 200;
+                this.x = 10;
+                this.y = this.gameHeight - this.height;
+                this.image = document.getElementById('playerImage');
+                this.frameX = 0;
+                this.maxFrame = 8;
+                this.frameY = 0;
+                this.fps = 20;
+                this.frameTimer = 0;
+                this.frameInterval = 1000 / this.fps;
+                this.speed = 0;
+                this.vy = 0;
+                this.weight = 1;
+            }
+            //dibujo de player
+        draw(context) {
+                //context.fillStyle = 'white';
+                //context.fillRect(this.x, this.y, this.width, this.height);
+                context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+            }
+            //movimiento de player
+        update(input, deltaTime) {
+                //animacion de perrito
+                if (this.frameTimer > this.frameInterval) {
+                    if (this.frameX >= this.maxFrame) this.frameX = 0;
+                    else this.frameX++;
+                    this.frameTimer = 0;
+                } else {
+                    this.frameTimer += deltaTime;
+                }
+                //movimiento
+                if (input.keys.indexOf('ArrowRight') > -1) {
+                    this.speed = 5;
+                } else if (input.keys.indexOf('ArrowLeft') > -1) {
+                    this.speed = -5;
+                } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()) {
+                    this.vy -= 32;
+                } else {
+                    this.speed = 0;
+                }
+                //movimiento horizontal
+                this.x += this.speed;
+                if (this.x < 0) this.x = 0;
+                else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+                //movimiento vertical
+                this.y += this.vy;
+                if (!this.onGround()) {
+                    this.vy += this.weight;
+                    this.maxFrame = 5;
+                    this.frameY = 1;
+                } else {
+                    this.vy = 0;
+                    this.maxFrame = 8;
+                    this.frameY = 0;
+                }
+                if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+
+            }
+            //devuelve al player al suelo 
+        onGround() {
+            return this.y >= this.gameHeight - this.height;
+        }
+    }
+    //class para fondo 
+    class Background {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById('backGoundImage');
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 720;
+            this.speed = 5;
+        }
+        draw(context) {
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height);
+        }
+        update(context) {
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) this.x = 0;
+        }
+    }
+    //class para enemigos
+    class Enemy {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 160;
+            this.height = 119;
+            this.image = document.getElementById('enemyImage');
+            this.x = this.gameWidth;
+            this.y = this.gameHeight - this.height;
+            this.frameX = 0;
+            this.maxFrame = 5;
+            this.fps = 20;
+            this.frameTimer = 0;
+            this.frameInterval = 1000 / this.fps;
+            this.speed = 8;
+        }
+        draw(context) {
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+
+        }
+        update(deltaTime) {
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.maxFrame) this.frameX = 0;
+                else this.frameX++;
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+            this.x -= this.speed;
+        }
+    }
+
+    //remover enemigos de la pantalla
+    function handleEnemies(deltaTime) {
+        if (enemyTimer > enemyInterval + randomEnemyInterval) {
+            enemies.push(new Enemy(canvas.width, canvas.height));
+            randomEnemyInterval = Math.random() * 1000 + 500;
+            enemyTimer = 0;
+        } else {
+            enemyTimer += deltaTime;
+        }
+        enemies.forEach(enemy => {
+            enemy.draw(ctx);
+            enemy.update(deltaTime);
+        })
+    }
+    //mensajes en pantalla
+    function displayStatusText() {
+
+    }
+    //instancias para las clases
+    const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
+    const background = new Background(canvas.width, canvas.height);
+    //timer para aparicion de enemigos 
+    let lasTime = 0;
+    let enemyTimer = 0;
+    let enemyInterval = 1000;
+    let randomEnemyInterval = Math.random() * 1000 + 500;
+
+    //animate principal
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lasTime;
+        lasTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        background.draw(ctx);
+        //background.update();
+        player.draw(ctx);
+        player.update(input, deltaTime);
+        handleEnemies(deltaTime);
+        requestAnimationFrame(animate);
+    }
+    animate(0);
+
+});
